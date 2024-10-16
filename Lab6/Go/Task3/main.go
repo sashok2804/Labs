9438039459790aae85cf3ffcedea8b441e286a58
sashok2804 <alex.0111127@gmail.com>
2024-10-16 13:00:33 +0400

    task 3 completed

--- Lab6/Go/Task3/main.go
+++ Lab6/Go/Task3/main.go
@@ -1,103 +1,50 @@
+// Задание:
+// Применение select для управления каналами:
+// Создайте две горутины, одна из которых будет генерировать случайные числа,
+// а другая — отправлять сообщения об их чётности/нечётности.
+// Используйте конструкцию select для приёма данных из обоих каналов и вывода результатов в консоль.
+// Продемонстрируйте, как select управляет многоканальными операциями.
+
 package main
 
 import (
-	"flag"
 	"fmt"
-	"math"
+	"math/rand"
+	"time"
 )
 
+// Генерация случайных чисел
+func generateNumbers(ch chan<- int) {
+	for {
+		num := rand.Intn(100)
+		ch <- num
+		time.Sleep(time.Second)
+	}
+}
+
+// Проверка на чётность/нечётность
+func checkEvenOdd(ch <-chan int, resultCh chan<- string) {
+	for {
+		num := <-ch
+		if num%2 == 0 {
+			resultCh <- fmt.Sprintf("Число %d чётное", num)
+		} else {
+			resultCh <- fmt.Sprintf("Число %d нечётное", num)
+		}
+	}
+}
+
 func main() {
+	numCh := make(chan int)
+	resultCh := make(chan string)
 
-	task := flag.Int("t", 1, "task num") // создаем флаг t, 1 - значение по умолчанию, "task num" - описание флага
+	go generateNumbers(numCh)
+	go checkEvenOdd(numCh, resultCh)
 
-	flag.Parse() // парсим флаги
-
-	switch *task {
-	case 1:
-		task1()
-	case 2:
-		task2()
-	case 3:
-		task3()
-	case 4:
-		task4()
-	default:
-		task1()
+	for {
+		select {
+		case result := <-resultCh:
+			fmt.Println(result)
+		}
 	}
-}
-func task1() {
-	p := Person{name: "Alice", age: 30}
-	p.Info()
-
-	p.Birthday()
-	p.Info()
-}
-
-func task2() {
-	c := Circle{radius: 5}
-	fmt.Printf("Circle Area: %.2f\n", c.Area())
-}
-
-func task3() {
-	c := Circle{radius: 5}
-	r := Rectangle{width: 4, height: 5}
-
-	shapes := []Shape{c, r}
-	PrintAreas(shapes)
-}
-func task4() {
-	b := Book{title: "Biblia", author: "IISYS"}
-	fmt.Println(b.String())
-}
-
-type Person struct {
-	name string
-	age  int
-}
-
-func (p Person) Info() {
-	fmt.Printf("Name: %s, Age: %d\n", p.name, p.age)
-}
-
-func (p *Person) Birthday() {
-	p.age++
-}
-
-type Circle struct {
-	radius float64
-}
-
-func (c Circle) Area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-type Rectangle struct {
-	width, height float64
-}
-
-func (r Rectangle) Area() float64 {
-	return r.width * r.height
-}
-
-type Shape interface {
-	Area() float64
-}
-
-func PrintAreas(shapes []Shape) {
-	for _, shape := range shapes {
-		fmt.Printf("Area: %.2f\n", shape.Area())
-	}
-}
-
-type Book struct {
-	title  string
-	author string
-}
-
-type Stringer interface {
-	String() string
-}
-
-func (b Book) String() string {
-	return fmt.Sprintf("Title: %s, Author: %s", b.title, b.author)
 }
